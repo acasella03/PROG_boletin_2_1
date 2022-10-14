96bcbcad9f66f76a2348b50bf5c560df9fc02c37
Angi Marliu Casella <acasella@danielcastelao.org>
2022-10-14 09:38:43 +0200

    version 1.0

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,13 @@
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    float resultado;
+
+    public static void main(String[] args) {
+        float base = 4f, altura = 3.0f, area;
+        area = base * altura / 2;
+        System.out.println("area = " + area);
+    }
+/*version 1.0 */
+}
